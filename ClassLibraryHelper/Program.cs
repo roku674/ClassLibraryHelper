59c22b895e445943ad722aa2396c13e82209df0b
roku674 <roku674@gmail.com>
2023-09-08 15:51:09 -0400

    Add project files.

---
+++ ClassLibraryHelper/Program.cs
@@ -0,0 +1,71 @@
+﻿//Created by Alexander Fields
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        string exePath = System.AppDomain.CurrentDomain.BaseDirectory;
+        string pathFile = System.IO.Path.Combine(exePath, "path.txt");
+
+        if (!System.IO.File.Exists(pathFile))
+        {
+            System.Console.WriteLine("path.txt not found in the application directory.");
+            return;
+        }
+
+        string rootPath = System.IO.File.ReadAllText(pathFile).Trim();
+
+        if (string.IsNullOrEmpty(rootPath) || !System.IO.Directory.Exists(rootPath))
+        {
+            System.Console.WriteLine("Invalid directory path in path.txt.");
+            return;
+        }
+
+        List<string> modifiedFiles = new List<string>();
+
+        Parallel.ForEach(System.IO.Directory.GetFiles(rootPath, "*.cs", System.IO.SearchOption.AllDirectories), file =>
+        {
+            if (ProcessFile(file))
+            {
+                lock (modifiedFiles)
+                {
+                    modifiedFiles.Add(file);
+                }
+            }
+        });
+
+        System.Console.WriteLine("Modified files:");
+        foreach (string file in modifiedFiles)
+        {
+            System.Console.WriteLine(Path.GetFileName(file));
+        }
+    }
+
+    static bool ProcessFile(string filePath)
+    {
+        string content = System.IO.File.ReadAllText(filePath);
+
+        // Regex pattern to match classes and variables without a summary above them
+        // This pattern also captures attributes above the class or variable declaration
+        string pattern = @"(?<!/// <summary>[\s\S]*?/// </summary>\s*)((\s*\[.*\]\s*)*)(public|private|protected|internal)(\s+static)?\s+(class|[\w<>]+\s+[\w]+(?=\s*(;|=|\{)))";
+
+        // Replacement pattern to add the summary
+        string replacement = @"/// <summary>
+    /// 
+    /// </summary>
+    $1$3$4 $5";
+
+        string newContent = Regex.Replace(content, pattern, replacement);
+
+        if (newContent != content)
+        {
+            System.IO.File.WriteAllText(filePath, newContent);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/cs1591?f1url=%3FappId%3Droslyn%26k%3Dk(CS1591)
+
+CS1591 I got frackin tired of 1000+ warnings for it and that's not an exxageration so I made this to help cure the insanity it was causing.
+
+Put in the folder path to Path.txt and it should do its thang 21 do yo thang
